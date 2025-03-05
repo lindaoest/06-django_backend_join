beabc4fdb07d11c0dc45fc8025c140a82a5f4a36
Linda Österreicher <lindaoest@gmail.com>
2025-03-05 22:24:53 +0100

    Refactor Task Serializer

--- join/api/serializers.py
+++ join/api/serializers.py
@@ -25,18 +25,28 @@ class CategorySerializer(serializers.ModelSerializer):
 
 class TaskSerializer(serializers.ModelSerializer):
 
-	assignedTo = ContactSerializer(many=True, read_only=True)
-	assignedTo_ids = serializers.PrimaryKeyRelatedField(
-		queryset = Contact.objects.all(),
-		many=True,
-		write_only=True,
-		source = 'assignedTo'
-	)
+	# assignedTo = ContactSerializer(many=True, read_only=True)
+	# assignedTo_ids = serializers.PrimaryKeyRelatedField(
+	# 	queryset = Contact.objects.all(),
+	# 	many=True,
+	# 	write_only=True,
+	# 	source = 'assignedTo'
+	# )
 	# assignedTo = serializers.PrimaryKeyRelatedField(queryset=Contact.objects.all(), many=True, write_only=True)  # Nur für die Eingabe (POST/PUT)
 	# contacts = serializers.StringRelatedField(source="assignedTo", many=True, read_only=True)
 
-	category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), write_only=True)
-	type = serializers.StringRelatedField(source="category", read_only=True)
+	# category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), write_only=True)
+	assignedTo = ContactSerializer(many=True)
+
+	def create(self, validated_data):
+		print(validated_data)
+		# contact_ids = validated_data.pop('assignedTo') # Extrahiere die Market-IDs und entferne sie aus validated_data
+		# tasks = Contact.objects.create(**validated_data) # Erstelle ein neues Seller-Objekt mit den restlichen Daten
+		# contacts = Contact.objects.filter(id__in=contact_ids) # Hole die Market-Objekte anhand der IDs
+		# seller.market.set(markets) # Weise die Märkte dem Seller zu (Many-to-Many Beziehung)
+		# return seller # Gib das erstellte Seller-Objekt zurück
+
+	category = serializers.StringRelatedField()
 
 	class Meta:
 		model = Task
