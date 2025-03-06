0b1a8dc85b8dc50e89ace7f5f6c959fb95f00755
Linda Österreicher <lindaoest@gmail.com>
2025-03-06 20:54:54 +0100

    Create relations in serializer

--- join/api/serializers.py
+++ join/api/serializers.py
@@ -1,22 +1,26 @@
 from rest_framework import serializers
 from ..models import Contact, Category, Task, Summary
 
-class ContactSerializer(serializers.Serializer):
-	name = serializers.CharField(max_length=366)
-	email = serializers.EmailField(max_length=366)
-	phone = serializers.IntegerField()
-	color = serializers.CharField()
+class ContactSerializer(serializers.ModelSerializer):
 
-	def create(self, validated_data):
-		return Contact.objects.create(**validated_data)
+	class Meta:
+		model = Contact
+		fields = ['id', 'name', 'email', 'phone', 'color']
+	# name = serializers.CharField(max_length=366)
+	# email = serializers.EmailField(max_length=366)
+	# phone = serializers.IntegerField()
+	# color = serializers.CharField()
 
-	def update(self, instance, validated_data):
-		instance.name = validated_data.get('name', instance.name)
-		instance.email = validated_data.get('email', instance.email)
-		instance.phone = validated_data.get('phone', instance.phone)
-		instance.color = validated_data.get('color', instance.color)
-		instance.save()
-		return instance
+	# def create(self, validated_data):
+	# 	return Contact.objects.create(**validated_data)
+
+	# def update(self, instance, validated_data):
+	# 	instance.name = validated_data.get('name', instance.name)
+	# 	instance.email = validated_data.get('email', instance.email)
+	# 	instance.phone = validated_data.get('phone', instance.phone)
+	# 	instance.color = validated_data.get('color', instance.color)
+	# 	instance.save()
+	# 	return instance
 
 class CategorySerializer(serializers.ModelSerializer):
 	class Meta:
@@ -24,29 +28,41 @@ class CategorySerializer(serializers.ModelSerializer):
 		fields = '__all__'
 
 class TaskSerializer(serializers.ModelSerializer):
+	assignedTo_ids = ContactSerializer(many=True, read_only=True)
+	assignedTo = serializers.ListField(
+		child=serializers.DictField(),
+		write_only=True
+	)
 
-	# assignedTo = ContactSerializer(many=True, read_only=True)
-	# assignedTo_ids = serializers.PrimaryKeyRelatedField(
-	# 	queryset = Contact.objects.all(),
-	# 	many=True,
-	# 	write_only=True,
-	# 	source = 'assignedTo'
-	# )
-	# assignedTo = serializers.PrimaryKeyRelatedField(queryset=Contact.objects.all(), many=True, write_only=True)  # Nur für die Eingabe (POST/PUT)
-	# contacts = serializers.StringRelatedField(source="assignedTo", many=True, read_only=True)
-
-	# category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), write_only=True)
-	assignedTo = ContactSerializer(many=True)
+	category = serializers.CharField(write_only=True)
+	category_id = serializers.IntegerField(source="category.id", read_only=True)
 
 	def create(self, validated_data):
-		print(validated_data)
+		print('category', validated_data)
+		contacts_data = validated_data.pop('assignedTo', [])
+		category_data = validated_data.pop('category', '')
+		assigned_contacts = []
+
+		for contact in contacts_data:
+			contact_id = Contact.objects.get(pk=contacts_data.id)
+			print('id', contact_id)
+			assigned_contacts.append(contact.get('id'))
+
+		category = Category.objects.get(title=category_data)
+
+		task = Task.objects.create(category=category, **validated_data)
+		task.assignedTo.set(assigned_contacts)
+
+		print('assigned_contacts', assigned_contacts)
+		print('task', task)
+
+		return task
 		# contact_ids = validated_data.pop('assignedTo') # Extrahiere die Market-IDs und entferne sie aus validated_data
 		# tasks = Contact.objects.create(**validated_data) # Erstelle ein neues Seller-Objekt mit den restlichen Daten
 		# contacts = Contact.objects.filter(id__in=contact_ids) # Hole die Market-Objekte anhand der IDs
 		# seller.market.set(markets) # Weise die Märkte dem Seller zu (Many-to-Many Beziehung)
 		# return seller # Gib das erstellte Seller-Objekt zurück
 
-	category = serializers.StringRelatedField()
 
 	class Meta:
 		model = Task
