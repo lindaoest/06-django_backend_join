da3e63594ae9c07599eaf703f1139455e50d34a2
Linda Österreicher <lindaoest@gmail.com>
2025-03-28 21:00:01 +0100

    Change auth url & clean TaskSerializer

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import RegistrationView, LoginView
 
 urlpatterns = [
-    path("registration/", RegistrationView.as_view()),
+    path("register/", RegistrationView.as_view()),
     path("login/", LoginView.as_view()),
 ]
--- join/api/serializers.py
+++ join/api/serializers.py
@@ -28,46 +28,21 @@ class CategorySerializer(serializers.ModelSerializer):
 		fields = '__all__'
 
 class TaskSerializer(serializers.ModelSerializer):
-	# assignedTo_ids = ContactSerializer(many=True, read_only=True)
-	# assignedTo = serializers.ListField(
-	# 	child=serializers.DictField(),
-	# 	write_only=True
-	# )
 
 	assignedTo = serializers.PrimaryKeyRelatedField(queryset=Contact.objects.all(),many=True, write_only=True)
-	# contacts = serializers.StringRelatedField(source='contact', many=True, read_only=True)
 	contacts = serializers.SerializerMethodField()
 	category = serializers.CharField()
-	# category_id = serializers.IntegerField(source="category.id", read_only=True)
 
 	def create(self, validated_data):
-		# print('category', validated_data)
 		contacts_data = validated_data.pop('assignedTo')
 		category_data = validated_data.pop('category', '')
-		# assigned_contacts = []
 
-		# for contact in contacts_data:
-		# 	contact_id = Contact.objects.get(id=contacts_data.id)
-		# 	print('id', contact_id)
-		# 	assigned_contacts.append(contact.get('id'))
-
-		category = Category.objects.get(title=category_data)
+		category, _ = Category.objects.get_or_create(title=category_data)
 
 		task = Task.objects.create(category=category, **validated_data)
 		task.assignedTo.set(contacts_data)
-		# task.contacts.set(contacts_data)
-
-		# print('assigned_contacts', assigned_contacts)
-		print('task', task)
 
 		return task
-		# contact_ids = validated_data.pop('assignedTo') # Extrahiere die Market-IDs und entferne sie aus validated_data
-		# tasks = Contact.objects.create(**validated_data) # Erstelle ein neues Seller-Objekt mit den restlichen Daten
-		# contacts = Contact.objects.filter(id__in=contact_ids) # Hole die Market-Objekte anhand der IDs
-		# seller.market.set(markets) # Weise die Märkte dem Seller zu (Many-to-Many Beziehung)
-		# return seller # Gib das erstellte Seller-Objekt zurück
-
-
 	class Meta:
 		model = Task
 		fields = ['id', 'category', 'title', 'description', 'date', 'priority', 'finishedSubtasks', 'subtasks', 'status', 'contacts', 'assignedTo']
