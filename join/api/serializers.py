d2d7889c06d5c1ad5fd28a5972cc218e4ac7b23f
Linda Österreicher <lindaoest@gmail.com>
2025-03-29 08:15:47 +0100

    Add update-method to TaskSerializer

--- join/api/serializers.py
+++ join/api/serializers.py
@@ -43,6 +43,27 @@ class TaskSerializer(serializers.ModelSerializer):
 		task.assignedTo.set(contacts_data)
 
 		return task
+
+	def update(self, instance, validated_data):
+		contacts_data = validated_data.pop('assignedTo')
+		category_data = validated_data.pop('category', '')
+
+		category = Category.objects.get(title=category_data)
+
+		instance.title = validated_data.get('title', instance.title)
+		instance.description = validated_data.get('description', instance.description)
+		instance.date = validated_data.get('date', instance.date)
+		instance.priority = validated_data.get('priority', instance.priority)
+		instance.category = category
+		instance.subtasks = validated_data.get('subtasks', instance.subtasks)
+		instance.finishedSubtasks = validated_data.get('finishedSubtasks', instance.finishedSubtasks)
+		instance.status = validated_data.get('status', instance.status)
+
+		instance.assignedTo.set(contacts_data)
+
+		instance.save()
+		return instance
+
 	class Meta:
 		model = Task
 		fields = ['id', 'category', 'title', 'description', 'date', 'priority', 'finishedSubtasks', 'subtasks', 'status', 'contacts', 'assignedTo']
